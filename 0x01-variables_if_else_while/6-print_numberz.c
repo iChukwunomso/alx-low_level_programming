6cb35e7b05373a898ee5fabca46c0c98ce3309bd
iChukwunomso <joshuanwanne@yahoo.com>
2023-08-14 15:30:45 -0700

    ./6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: A lways 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 10 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
