dec5bc74d9c48eaa5b9bd3fd289313a7d476974b
iChukwunomso <joshuanwanne@yahoo.com>
2023-09-26 10:41:09 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint - a function to free a linked list
+ *
+ * @head: listint_t list to be freed
+ *
+ * Return: NULL
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
+
